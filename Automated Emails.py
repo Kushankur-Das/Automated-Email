1de38fda3d5fe40879682ce6b0e10e0e49f46f76
Kushankur Das <kushankurdas53543@gmail.com>
2020-10-17 04:11:26 +0530

    Hi.. This is Kushankur Das... I have attatched my code here.

--- Automated Emails.py
+++ Automated Emails.py
@@ -6,7 +6,7 @@ import smtplib as s
 ob=s.SMTP("smtp.gmail.com",587)
 ob.starttls()
 ob.login("*Your gmail id you want to use*","*Your password*")
-subject="*Write the subject to your email"
+subject="*Write the subject to your email*"
 body="*Write down the message*"
 message="Subject:{}\n\n{}".format(subject,body)
 listOfAddress=["*Recipient 1 *","*Recipient 2*"....]  #Add as many as you want
