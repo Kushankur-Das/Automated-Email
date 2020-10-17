0296224397142ad3cfa401fd0e0c6d6ab1b026d7
Kushankur Das <64121196+Kushankur-Das@users.noreply.github.com>
2020-10-17 07:59:11 +0530

    Update Automated Emails.py

--- Automated Emails.py
+++ Automated Emails.py
@@ -1,6 +1,6 @@
 # Hi This is Kushankur Das. This is my project on sending an email to various people with just one click from the terminal.
 # You don't need to install any package... Just make sure you are using the latest version of python 3.
-# You need to turn on less secure apps option towards your google account. Copy and paste the link next into your browser with the sender google account signed in.
+# You need to turn on less secure apps option towards your google account. Copy and paste the link next into your browser with the sender google account signed in.https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4PP0cAqCpE4prwc-aJRg51eR9YNmwZIOlYKPINEvGMqeb5pR7MJ1_jcuf4EYm9feJZBuM2Ke3ZOhSvFEAKu9OXfcM78Zw
 #And you are good to go.
 import smtplib as s
 ob=s.SMTP("smtp.gmail.com",587)
